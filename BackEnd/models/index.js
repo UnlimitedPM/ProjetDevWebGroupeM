2a9811518555bd1b5f176ebcf7e55a36ef56490d
Mike <mukamtallamikeryan@gmail.com>
2025-12-13 00:29:15 +0100

    Ajout de la relation many-to-many entre User et Event

--- BackEnd/models/index.js
+++ BackEnd/models/index.js
@@ -21,22 +21,34 @@ User.hasMany(Event, {
 
 // An event has one category
 Event.belongsTo(Category, {
-  foreignKey: 'categoryId', // Nom explicite de la clé
+  foreignKey: "categoryId", // Nom explicite de la clé
   as: "category",
 });
 Category.hasMany(Event, {
-  foreignKey: 'categoryId', // Nom explicite de la clé
+  foreignKey: "categoryId", // Nom explicite de la clé
   as: "events",
 });
 
 // An event has one venue
 Event.belongsTo(Venue, {
-  foreignKey: 'venueId', // Nom explicite de la clé
+  foreignKey: "venueId", // Nom explicite de la clé
   as: "venue",
 });
 Venue.hasMany(Event, {
-  foreignKey: 'venueId', // Nom explicite de la clé
+  foreignKey: "venueId", // Nom explicite de la clé
   as: "events",
+});
+
+// Many-to-Many: Users can register to many events, events can have many participants
+Event.belongsToMany(User, {
+  through: "event_registration",
+  as: "participants",
+  foreignKey: "eventId",
+});
+User.belongsToMany(Event, {
+  through: "event_registration",
+  as: "registeredEvents",
+  foreignKey: "userId",
 });
 
 module.exports = {
